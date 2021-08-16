7918ea9c0b3d97065a3fe06905bb2f2ba7ee79cf
Eric Rowell <ericrowell@Erics-MacBook-Pro-2.local>
2021-08-15 20:47:42 -0700

    fixed webgl normal index issue

--- src/scene.js
+++ src/scene.js
@@ -31,15 +31,11 @@ function scene_init() {
 };
 
 function scene_initUniforms() {
-  webgl_setAttribLocation(shaderProgram, sceneContext, 've');
-  webgl_setAttribLocation(shaderProgram, sceneContext, 'no');
-  webgl_setAttribLocation(shaderProgram, sceneContext, 'tc');
+  webgl_setAttribLocation(shaderProgram, sceneContext, 've'); // vertex vector
+  webgl_setAttribLocation(shaderProgram, sceneContext, 'tc'); // texture vector
 
-  webgl_setUniformLocation(shaderProgram, sceneContext, 'pm');
-  webgl_setUniformLocation(shaderProgram, sceneContext, 'mv');
-  webgl_setUniformLocation(shaderProgram, sceneContext, 'sa');
-  webgl_setUniformLocation(shaderProgram, sceneContext, 'nm');
-  webgl_setUniformLocation(shaderProgram, sceneContext, 'fl');
+  webgl_setUniformLocation(shaderProgram, sceneContext, 'mv'); // move matrix
+  webgl_setUniformLocation(shaderProgram, sceneContext, 'pm'); // perspective matrix
 };
 
 function scene_setUniforms() {
@@ -47,12 +43,11 @@ function scene_setUniforms() {
   sceneContext.uniformMatrix4fv(shaderProgram.pm, false, pMatrix);
   sceneContext.uniformMatrix4fv(shaderProgram.mv, false, mvMatrix);
   
-  let normalMatrix = mat3.create();
-  mat4.toInverseMat3(mvMatrix, normalMatrix);
-  mat3.transpose(normalMatrix);
-  sceneContext.uniformMatrix3fv(shaderProgram.nm, false, normalMatrix);
-
-  sceneContext.uniform1i(shaderProgram.fl, flashTimeRemaining > 0);
+  // let normalMatrix = mat3.create();
+  // mat4.toInverseMat3(mvMatrix, normalMatrix);
+  // mat3.transpose(normalMatrix);
+  // sceneContext.uniformMatrix3fv(shaderProgram.nm, false, normalMatrix);
+  // sceneContext.uniform1i(shaderProgram.fl, flashTimeRemaining > 0);
 };
 
 function scene_render(buffers, texture) {
@@ -65,12 +60,13 @@ function scene_render(buffers, texture) {
   sceneContext.vertexAttribPointer(shaderProgram.tc, 2, sceneContext.FLOAT, false, 0, 0);
   sceneContext.activeTexture(sceneContext.TEXTURE0);
   sceneContext.bindTexture(sceneContext.TEXTURE_2D, texture);
+
   // index buffers
   sceneContext.bindBuffer(sceneContext.ELEMENT_ARRAY_BUFFER, buffers.index);
 
   // normal buffers
-  sceneContext.bindBuffer(sceneContext.ARRAY_BUFFER, buffers.normal);
-  sceneContext.vertexAttribPointer(shaderProgram.no, 3, sceneContext.FLOAT, false, 0, 0);
+  //sceneContext.bindBuffer(sceneContext.ARRAY_BUFFER, buffers.normal);
+  //sceneContext.vertexAttribPointer(shaderProgram.no, 3, sceneContext.FLOAT, false, 0, 0);
 
 
 
